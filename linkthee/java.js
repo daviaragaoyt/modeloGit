46a034bbef107203709cb5e07d1c2330fa1f4e54
DAVI <daviaragaoyt@gmail.com>
2023-07-14 14:11:48 -0300

    tetando

---
+++ linkthee/java.js
@@ -0,0 +1,91 @@
+console.log("Ola, Seja Bem Vind0.");
+console.log("Este é o meu primeiro site teste e foi feito para uma amiga");
+console.log("Aqui voce encontra sua foto com seus meios de contato");
+
+const form = document.querySelector("#form");
+const nameInput = document.querySelector("#name");
+const emailInput = document.querySelector("email");
+const senhaInput = document.querySelector("senha");
+
+function submitFormulario(event) {
+    event.preventDefault();
+
+    //Verifica se o nome esta vazio
+    // if (event.srcElement[0].value === "") {
+    //     alert("Por favor, preecha seu nome");
+    //     return;
+    // }
+
+    // //verifica a data 
+    // if (event.srcElement[1].value === "") {
+    //     alert("Por favor, preencha sua data de nascimento");
+    //     return;
+    // }
+
+    // //Verifica se o email esta preenchido
+    // if (event.srcElement[2].value === "") {
+    //     alert("Por favor, preencha com seu email");
+    //     return;
+    // }
+
+    // //Verificar se a está preenchida
+    // if (validaPassword(event.srcElement[3].value, 8)) {
+    //     alert("A senha precisa de 8 digitos minimos ");
+    //     return;
+    // }
+
+    string.link("google.com")
+}
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    console.log('galinha', event)
+
+    //Verifica se o nome esta vazio
+    if (nameInput.value === "" || nameInput.value === null) {
+        alert("Por favor, preecha seu nome");
+        return;
+    }
+
+    //Verifica se o email esta preenchido
+    if (emailInput.value === "" || isEmailValid(emailInput.value)) {
+        alert("Por favor, preencha com seu email");
+        return;
+    }
+    //Verificar se a está preenchida
+    if (!validaPassword(senhaInput.value, 8)) {
+        alert("A senha precisa de 8 digitos minimos ");
+        return;
+    }
+
+
+    //Se tudo estiver devidamente preenchido, envie o formulario
+    form.submit();
+});
+//cria um regex para validar email
+function isEmailValid(email) {
+
+    const emailRegex = new RegExp(
+        //usuario@gmail.com
+        /^[a-zA-Z0-9,_-]+@[a-zA-Z0-9,_-]+\.[a-zA-Z]{2,}$/
+    );
+
+    if (emailRegex.teste(email)) {
+        return true;
+    }
+
+    return false;
+}
+
+//Função que valida a senha
+
+function validaPassword(senha, minDigits) {
+
+    if (senha.length >= minDigits) {
+        return false
+    }
+    //Senhas invalidas
+    return true
+}
+
