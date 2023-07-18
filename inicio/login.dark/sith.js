f52076f19b2ed6fee642942b18730b2ec9ed4136
DAVI <daviaragaoyt@gmail.com>
2023-07-18 16:23:55 -0300

    Star wars 100%

--- inicio/login.dark/sith.js
+++ inicio/login.dark/sith.js
@@ -8,58 +8,40 @@ const emailInput = document.querySelector("email");
 const senhaInput = document.querySelector("senha");
 
 function submitFormulario(event) {
+    
+    event.preventDefault()
+    console.log('galinha:', event);
 
     // Verifica se o nome esta vazio
     if (event.srcElement[0].value === "") {
+        event.preventDefault()
         alert("Por favor, preecha seu nome");
         return;
     }
 
     //verifica a data 
     if (event.srcElement[1].value === "") {
+        event.preventDefault()
         alert("Por favor, preencha sua data de nascimento");
         return;
     }
 
     //Verifica se o email esta preenchido
     if (event.srcElement[2].value === "") {
+        event.preventDefault()
         alert("Por favor, preencha com seu email");
         return;
     }
 
     //Verificar se a está preenchida
     if (validaPassword(event.srcElement[3].value, 8)) {
+        event.preventDefault()
         alert("A senha precisa de 8 digitos minimos ");
-      return;
+        return;
     }
 }
 
-form.addEventListener("submit", (event) => {
-    event.preventDefault();
 
-    console.log('galinha', event)
-
-    //Verifica se o nome esta vazio
-    if (nameInput.value === "" || nameInput.value === null) {
-        alert("Por favor, preecha seu nome");
-        return;
-    }
-
-    //Verifica se o email esta preenchido
-    if (emailInput.value === "" || isEmailValid(emailInput.value)) {
-        alert("Por favor, preencha com seu email");
-        return;
-    }
-    //Verificar se a está preenchida
-    if (!validaPassword(senhaInput.value, 8)) {
-        alert("A senha precisa de 8 digitos minimos ");
-        return;
-    }
-
-
-    //Se tudo estiver devidamente preenchido, envie o formulario
-    form.submit();
-});
 //cria um regex para validar email
 function isEmailValid(email) {
 
--- inicio/script.js
+++ inicio/script.js
@@ -5,15 +5,32 @@ const selectInput = document.querySelector("#name");
 const jediInput = document.querySelector("email");
 const sithInput = document.querySelector("senha");
 
-function redirecionar(opcoes) {
-    var select = document.getElementById("opcoes");
-    var opcaoSelecionada = select.value;
-    
-    if (opcaoSelecionada === "") {
-      alert("Por favor, selecione uma opção antes de prosseguir.");
-    } else {
-      // Redirecionar para a próxima página
-      window.location.href = "proxima_pagina.html";
-    }
+function alterarClass() {
+  if (document.getElementById("opcoes").value == "jedi") {
+    document.getElementById("bloco").className = "containerJedi";
+    document.getElementById("botaoEnviar").className = "enviarJedi";
+
+    document.getElementById('form').action = "./login.jedi/jedi.html"
   }
+
+  if (document.getElementById("opcoes").value == "sith") {
+    document.getElementById("bloco").className = "containerSith";
+    document.getElementById("botaoEnviar").className = "enviarSith";
+
+    document.getElementById('form').action = "./login.dark/sith.html"
+  }
+}
+
+function redirecionar(opcoes) {
+  var select = document.getElementById("opcoes");
+  var opcaoSelecionada = select.value;
+
+  if (opcaoSelecionada === "") {
+    event.preventDefault();
+    alert("Por favor, selecion e uma opção antes de prosseguir.");
+  } else {
+    // Redirecionar para a próxima página
+    window.location.href = "google.com";
+  }
+}
 
