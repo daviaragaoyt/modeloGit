9bbfb9438b790e27ff9662487b12831148cc7066
DAVI <daviaragaoyt@gmail.com>
2023-07-18 13:45:30 -0300

    Projeto quase completo

---
+++ inicio/inicio.js
@@ -0,0 +1,17 @@
+const form = document.querySelector("#form");
+const selectInput = document.querySelector("#name");
+const jediInput = document.querySelector("email");
+const sithInput = document.querySelector("senha");
+
+function redirecionar(opcoes) {
+    var select = document.getElementById("opcoes");
+    var opcaoSelecionada = select.value;
+    
+    if (opcaoSelecionada === "") {
+      alert("Por favor, selecione uma opção antes de prosseguir.");
+    } else {
+      // Redirecionar para a próxima página
+      window.location.href = "proxima_pagina.html";
+    }
+  }
+
