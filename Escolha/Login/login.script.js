c4213739abc34623269061cdcab907a693ac9275
DAVI <daviaragaoyt@gmail.com>
2023-07-18 08:42:43 -0300

    Criando a escolha JEDI

--- Escolha/Login/login.script.js
+++ Escolha/Login/login.script.js
@@ -2,6 +2,8 @@ console.log("Ola, Seja Bem Vind0.");
 console.log("Este é o meu primeiro site teste e foi feito para uma amiga");
 console.log("Aqui voce encontra sua foto com seus meios de contato");
 
+/*alert("Obrigado por acessar!");*/
+
 const form = document.querySelector("#form");
 const nameInput = document.querySelector("#name");
 const emailInput = document.querySelector("email");
